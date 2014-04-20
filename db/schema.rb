65c7d197f5033a41f3e694522d04970b8e71c4c3
Jonathan Chang <jonoschang7@gmail.com>
2014-04-20 18:11:34 -0400

    add index to users email to for uniqueness and make email downcase before save

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420182417) do
+ActiveRecord::Schema.define(version: 20140420220902) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20140420182417) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
